65e68b9b91f299c6d07f0e651d17a29a578f1eed
kyletan214 <kyletan214@gmail.com>
2016-05-24 16:32:13 +0800

    Add files via upload

---
+++ hello2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main ()
+
+{
+	printf("@ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @\n");
+	printf(" Kyle Vincent Siy Tan\n BS CHE \n 12345678 \n May 24, 2016 \n lorewalkercho@google.com \n 09172581478\n");
+	printf("@ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @\n");
+	system("PAUSE");
+	return 0;
+}
